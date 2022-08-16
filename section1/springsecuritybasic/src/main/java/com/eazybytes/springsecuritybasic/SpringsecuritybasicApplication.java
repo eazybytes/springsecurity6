eddfea3e0d6026561b8ed6bae30ce6215adb4507
eazybytes <tutor@eazybytes.com>
2022-08-16 18:04:25 +0530

    Create a simple Spring security project & secure Rest service

---
+++ section1/springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.eazybytes.springsecuritybasic.controller") // Optional
+public class SpringsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+	}
+
+}
