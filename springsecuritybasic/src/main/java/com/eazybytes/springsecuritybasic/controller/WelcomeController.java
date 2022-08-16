e260f141b7a1263bb61f7a3bcefa564f3a9b5498
eazybytes <tutor@eazybytes.com>
2022-08-16 14:55:50 +0530

    Create a simple Spring security project & secure Rest service

---
+++ springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome from Spring Application with Security";
+    }
+
+}
---
+++ springsecuritybasic/src/test/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
