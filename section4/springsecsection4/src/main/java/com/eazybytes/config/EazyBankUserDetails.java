45bb3a7651e16e933ed9093fa7bea9def85f23c2
eazybytes <tutor@eazybytes.com>
2022-08-20 16:10:27 +0530

    Password management using PasswordEncoders

--- section4/springsecsection4/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
+++ section4/springsecsection4/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
@@ -1,14 +1,17 @@
 package com.eazybytes.config;
 
 import com.eazybytes.model.Customer;
-import com.eazybytes.model.SecurityCustomer;
 import com.eazybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,11 +22,18 @@ public class EazyBankUserDetails implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String userName, password = null;
+        List<GrantedAuthority> authorities = null;
         List<Customer> customer = customerRepository.findByEmail(username);
         if (customer.size() == 0) {
             throw new UsernameNotFoundException("User details not found for the user : " + username);
+        } else{
+            userName = customer.get(0).getEmail();
+            password = customer.get(0).getPwd();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
         }
-        return new SecurityCustomer(customer.get(0));
+        return new User(username,password,authorities);
     }
 
 }
