57b99f370ddef8fa188797b162302cbb2da45e6f
eazybytes <tutor@eazybytes.com>
2022-08-23 18:17:54 +0530

    Implement Authorization using Authorities,Roles

--- section7/springsecsection7/src/main/java/com/eazybytes/controller/NoticesController.java
+++ section7/springsecsection7/src/main/java/com/eazybytes/controller/NoticesController.java
@@ -1,8 +1,11 @@
 package com.eazybytes.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +19,12 @@ public class NoticesController {
 	private NoticeRepository noticeRepository;
 	
 	@GetMapping("/notices")
-	public List<Notice> getNotices() {
+	public ResponseEntity<List<Notice>> getNotices() {
 		List<Notice> notices = noticeRepository.findAllActiveNotices();
 		if (notices != null ) {
-			return notices;
+			return ResponseEntity.ok()
+					.cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+					.body(notices);
 		}else {
 			return null;
 		}
