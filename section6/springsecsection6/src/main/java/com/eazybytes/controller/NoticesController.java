c2fbfd3bc5f8cae47e482242ba113e97c3305075
eazybytes <tutor@eazybytes.com>
2022-08-23 18:17:23 +0530

    Understand & handle CORs & CSRF issues inside Web app

--- section6/springsecsection6/src/main/java/com/eazybytes/controller/NoticesController.java
+++ section6/springsecsection6/src/main/java/com/eazybytes/controller/NoticesController.java
@@ -1,8 +1,11 @@
 package com.eazybytes.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,14 @@ public class NoticesController {
 	
 	@Autowired
 	private NoticeRepository noticeRepository;
-	
+
 	@GetMapping("/notices")
-	public List<Notice> getNotices() {
+	public ResponseEntity<List<Notice>> getNotices() {
 		List<Notice> notices = noticeRepository.findAllActiveNotices();
 		if (notices != null ) {
-			return notices;
+			return ResponseEntity.ok()
+					.cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+					.body(notices);
 		}else {
 			return null;
 		}
