18f1be8737947638ac254540c20add8cbc8a851b
eazybytes <tutor@eazybytes.com>
2022-10-22 09:14:09 +0530

    Define Users, Security using InMemoryUserDetailsManager,JdbcUserDetailsManager & UserDetailsService

--- section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -20,9 +20,9 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                        .authorizeRequests()
-                        .antMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .antMatchers("/notices","/contact","/register").permitAll()
+                        .authorizeHttpRequests()
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
