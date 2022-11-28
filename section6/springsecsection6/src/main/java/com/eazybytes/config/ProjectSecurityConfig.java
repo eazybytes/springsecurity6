14705b9c2c4d37b2863affd27b9d71f52378fa5c
eazybytes <tutor@eazybytes.com>
2022-11-28 20:10:34 +0530

    Understand & handle CORs & CSRF issues inside Web app

--- section6/springsecsection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section6/springsecsection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -22,6 +22,13 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /**
+         *  From Spring Security 6, below actions will not happen by default,
+         *  1) The Authentication details will not be saved automatically into SecurityContextHolder. To change this behaviour either we need to save
+         *      these details explicitly into SecurityContextHolder or we can configure securityContext().requireExplicitSave(false) like shown below.
+         *  2) The Session & JSessionID will not be created by default. Inorder to create a session after intial login, we need to configure
+         *      sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)) like shown below.
+         */
         http.securityContext().requireExplicitSave(false)
                 .and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .cors().configurationSource(new CorsConfigurationSource() {
@@ -35,6 +42,13 @@ public class ProjectSecurityConfig {
                 config.setMaxAge(3600L);
                 return config;
             }
+                    /**
+                     *  From Spring Security 6, by default the CSRF Cookie that got generated during intial login will not be shared to UI application.
+                     *  The developer has to write logic to read the CSRF token and send it as part of the response. When framework sees the
+                     *  CSRF token in the response header, it takes of sending the same as Cookie as well. For the same, I have created
+                     *  a filter with the name CsrfCookieFilter and configured the same to run every time after the Spring Security in built filter
+                     *  BasicAuthenticationFilter like shown below. More details about Filters, are discussed inside the Section 8 of the course.
+                     */
         }).and().csrf().ignoringRequestMatchers("/contact","/register").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and().addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
