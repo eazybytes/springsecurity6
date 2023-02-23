039901d872487b7fb86b9c499f5ff19a3b8b9dc4
eazybytes <tutor@eazybytes.com>
2023-02-23 13:49:27 +0530

    Define Users, Security using InMemoryUserDetailsManager,JdbcUserDetailsManager & UserDetailsService

--- section3/springsecsection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section3/springsecsection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,8 +2,6 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class EazyBankBackendApplication {
