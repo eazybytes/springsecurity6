054f6d71ee2091147775b48646499fba5cbdff2f
eazybytes <tutor@eazybytes.com>
2023-05-26 17:11:36 +0530

    Custom Security Configurations, PermitAll, DenyAll Configurations

--- section2/springsecsection2/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section2/springsecsection2/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,27 +16,26 @@ public class ProjectSecurityConfig {
          *  Below is the custom security configurations
          */
 
-        http.authorizeHttpRequests()
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
 
         /**
          *  Configuration to deny all the requests
          */
-        /*http.authorizeHttpRequests().anyRequest().denyAll()
-                .and().formLogin()
-                .and().httpBasic();
+        /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();*/
 
         /**
          *  Configuration to permit all the requests
          */
-        /*http.authorizeHttpRequests().anyRequest().permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+        /*http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();*/
     }
 
