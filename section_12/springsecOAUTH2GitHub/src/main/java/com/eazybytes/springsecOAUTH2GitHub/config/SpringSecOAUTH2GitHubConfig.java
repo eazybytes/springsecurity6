d28581f23d0b0157fa134768a33c5cc74a007685
eazybytes <tutor@eazybytes.com>
2023-05-26 19:02:20 +0530

    OAUTH2 demo using Github Auth server

--- section_12/springsecOAUTH2GitHub/src/main/java/com/eazybytes/springsecOAUTH2GitHub/config/SpringSecOAUTH2GitHubConfig.java
+++ section_12/springsecOAUTH2GitHub/src/main/java/com/eazybytes/springsecOAUTH2GitHub/config/SpringSecOAUTH2GitHubConfig.java
@@ -2,6 +2,7 @@ package com.eazybytes.springsecOAUTH2GitHub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,7 +12,8 @@ public class SpringSecOAUTH2GitHubConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login();
+        http.authorizeHttpRequests((requests)->requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
